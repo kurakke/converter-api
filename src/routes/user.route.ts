72afb196939acaa3734cd770c5d0c8e6a5a3b823
kurakke <kcanggua@gmail.com>
2023-04-20 15:46:49 +0900

    userのrouterをexportした

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -3,3 +3,7 @@ import express from "express";
 const router = express.Router();
 const userController = require('../controllers/user.controller');
 
+router.post('/getById', userController.getById);
+router.post('/create', userController.create);
+
+module.exports = router;
